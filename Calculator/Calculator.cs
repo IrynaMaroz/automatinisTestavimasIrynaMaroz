139835634dfdf0010cab4491b0b7fd1b3dda77a5
IrynaMaroz <maroz.iryna@gmail.com>
2023-11-04 17:47:39 +0200

    Refactor calculator app

--- Calculator/Calculator.cs
+++ Calculator/Calculator.cs
@@ -1,26 +1,25 @@
 ﻿namespace Calculator
 {
-    internal class Calculator
+    public class Methods
     {
-        static internal int Sum(int number1, int number2)
+        static public int Sum(int number1, int number2)
         {
             return number1 + number2;
         }
 
-        static internal int Subtract(int number1, int number2)
+        static public int Subtract(int number1, int number2)
         {
             return number1 - number2;
         }
 
-        static internal int Multiply(int number1, int number2)
+        static public int Multiply(int number1, int number2)
         {
             return number1 * number2;
         }
 
-        static internal double Divide(int number1, int number2)
+        static public double Divide(int number1, int number2)
         {
             return number1 / number2;
         }
-
     }
 }
