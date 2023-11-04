56797ca9488237adf5bf419ff4ac2bbf7a6b5567
IrynaMaroz <maroz.iryna@gmail.com>
2023-11-04 17:49:38 +0200

    Add demo calculator unit tests

---
+++ CalculatorUnitTests/Tests.cs
@@ -0,0 +1,39 @@
+﻿using Calculator;
+using NUnit.Framework;
+
+namespace CalculatorUnitTests
+{
+    public class Tests
+    {
+        [Test]
+        public void TestSum() 
+        {
+            // Arrange (Testo duomenys)
+            int number1 = 5;
+            int number2 = 2;
+            int expectedResult = 7;
+
+            // Act (Testo veiksmai)
+            int actualResult = Methods.Sum(number1, number2);
+
+            // Assert (Testo rezultatu palyginimas)
+            Assert.AreEqual(expectedResult, actualResult);
+            //Assert.IsTrue(actualResult == expectedResult);
+        }
+        [Test]
+        public void TestDivision()
+        {
+            // Arrange (Testo duomenys)
+            int number1 = 5;
+            int number2 = 2;
+            double expectedResult = 2.5;
+
+            // Act (Testo veiksmai)
+            double actualResult = Methods.Divide(number1, number2);
+
+            // Assert (Testo rezultatu palyginimas)
+            Assert.AreEqual(expectedResult, actualResult);
+            //Assert.IsTrue(actualResult == expectedResult);
+        }
+    }
+}
