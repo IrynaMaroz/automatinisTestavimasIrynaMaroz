a0086a9d5d2cc182d148032e6e026c115a26b074
IrynaMaroz <maroz.iryna@gmail.com>
2023-11-05 21:04:18 +0200

    uzduotis 18 1 atvejis atliktas testas

---
+++ Užduotis18SeleniumTestai/1 atvejis.cs
@@ -0,0 +1,42 @@
+﻿
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Linq;
+
+namespace Užduotis18SeleniumTestai
+{
+    public class Class1
+    {
+        [Test]
+        public void DemoTest()
+        {
+            string expectedResultEmail = "Email:name@example.com";
+            string expectedResultName = "Name:Iryna";
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "https://demoqa.com/text-box";
+
+            IWebElement inputName = driver.FindElement(By.XPath("//*[@id=\"userName\"]"));
+            inputName.SendKeys("Iryna");
+
+            IWebElement inputEmail = driver.FindElement(By.XPath("//*[@id=\"userEmail\"]"));
+            inputEmail.SendKeys("name@example.com");
+
+            IWebElement inputCurrentAddress = driver.FindElement(By.XPath("//*[@id=\"currentAddress\"]"));
+            inputCurrentAddress.SendKeys("Vilnius");
+
+            IWebElement inputPermanentAddress = driver.FindElement(By.XPath("//*[@id=\"permanentAddress\"]"));
+            inputPermanentAddress.SendKeys("Kaunas");
+
+            IWebElement submit = driver.FindElement(By.XPath("//*[@id=\"submit\"]"));
+          
+            submit.Click();
+            var elementOutputEmail = driver.FindElement(By.XPath("//*[@id=\"email\"]"));
+            var elementOutputName = driver.FindElement(By.XPath("//*[@id=\"name\"]"));
+            string actualResultEmail = elementOutputEmail.Text;
+            string actualResultName = elementOutputName.Text;
+            driver.Quit();
+            Assert.AreEqual(expectedResultEmail, actualResultEmail);
+        }
+    }
+}
--- seleniumTestsWithoutPOM/GoogleTests.cs
+++ seleniumTestsWithoutPOM/GoogleTests.cs
@@ -9,8 +9,25 @@ namespace seleniumTestsWithoutPOM
         [Test]
         public void DemoTest() 
         {
+            string expectedResult = "Hello World - Google Search";
+
             IWebDriver driver = new ChromeDriver();
-            driver.Url = "http://www.google.com";    
+            driver.Url = "http://www.google.com";
+
+            IWebElement buttonDeclineCookies = driver.FindElement(By.XPath("//*[@id=\"W0wltc\"]"));
+            buttonDeclineCookies.Click();
+
+            IWebElement inputSearch = driver.FindElement(By.XPath("//*[@id=\"APjFqb\"]"));
+            inputSearch.SendKeys("Hello World");
+
+            IWebElement buttonSearch = driver.FindElement(By.XPath("(//*[@name=\"btnK\"])[2]"));
+            buttonSearch.Click();
+
+            string actualResult = driver.Title;
+
+            driver.Quit();
+
+            Assert.AreEqual(expectedResult, actualResult);
         }
     }
 }
