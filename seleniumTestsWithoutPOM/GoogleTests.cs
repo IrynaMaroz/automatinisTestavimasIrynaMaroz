97f9595cd0b4deed453153c8177c8b725a0fd782
IrynaMaroz <maroz.iryna@gmail.com>
2023-11-05 14:44:06 +0200

    Selenium demo test

---
+++ seleniumTestsWithoutPOM/GoogleTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace seleniumTestsWithoutPOM
+{
+    public class GoogleTests
+    {
+        [Test]
+        public void DemoTest() 
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "http://www.google.com";    
+        }
+    }
+}
