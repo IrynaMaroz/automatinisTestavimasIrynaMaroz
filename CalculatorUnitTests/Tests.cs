681d8df6b32238afe30f90b54ec0466419282d6a
IrynaMaroz <maroz.iryna@gmail.com>
2023-11-05 09:57:23 +0200

    updated tests

--- CalculatorUnitTests/Tests.cs
+++ CalculatorUnitTests/Tests.cs
@@ -20,20 +20,14 @@ namespace CalculatorUnitTests
             Assert.AreEqual(expectedResult, actualResult);
             //Assert.IsTrue(actualResult == expectedResult);
         }
-        [Test]
-        public void TestDivision()
+        [TestCase (5, 2, 2.5)]
+        [TestCase(7, 2, 3.5)]
+        [TestCase(1, 3, 0.3333)]
+        [TestCase(1, 8, 0.125)]
+        public void TestDivision(int number1, int number2, double expectedResult)
         {
-            // Arrange (Testo duomenys)
-            int number1 = 5;
-            int number2 = 2;
-            double expectedResult = 2.5;
-
-            // Act (Testo veiksmai)
-            double actualResult = Methods.Divide(number1, number2);
-
-            // Assert (Testo rezultatu palyginimas)
+            double actualResult = Methods.Divide(number1, number2);        
             Assert.AreEqual(expectedResult, actualResult);
-            //Assert.IsTrue(actualResult == expectedResult);
         }
     }
 }
